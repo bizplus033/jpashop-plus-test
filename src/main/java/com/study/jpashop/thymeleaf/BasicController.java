9cbe941ede0a48ba8e1e6df21420daf5dd76645c
bizplus033 <bizplus033@naver.com>
2024-01-12 14:24:23 +0900

    [feature/thymeleaf-basic] 속성

--- src/main/java/com/study/jpashop/thymeleaf/BasicController.java
+++ src/main/java/com/study/jpashop/thymeleaf/BasicController.java
@@ -123,4 +123,12 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "thymeleaf/basic/operation";
     }
+
+    /**
+     * 속성 값 설정
+     */
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "thymeleaf/basic/attribute";
+    }
 }
