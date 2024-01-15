a1ec2c859881936c4c8c855669feb4b2ecb899a9
bizplus033 <bizplus033@naver.com>
2024-01-15 16:31:15 +0900

    [feature/thymeleaf-basic] 템플릿 조각

---
+++ src/main/java/com/study/jpashop/thymeleaf/TemplateController.java
@@ -0,0 +1,14 @@
+package com.study.jpashop.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+    @GetMapping("/fragment")
+    public String template() {
+        return "thymeleaf/template/fragment/fragmentMain";
+    }
+}
