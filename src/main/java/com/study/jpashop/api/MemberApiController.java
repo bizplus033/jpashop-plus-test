6fb0b05cfcbf0e40d0826cadd8c7bb566fb18c7f
bizplus033 <bizplus033@naver.com>
2024-02-13 17:13:53 +0900

    [jpa2/API 개발 기본기본] 회원 등록 API - V1

---
+++ src/main/java/com/study/jpashop/api/MemberApiController.java
@@ -0,0 +1,36 @@
+package com.study.jpashop.api;
+
+import com.study.jpashop.domain.Member;
+import com.study.jpashop.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    public static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+}
