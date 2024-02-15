c30267b2ac4fc089e706d104e74b72019a916d2c
bizplus033 <bizplus033@naver.com>
2024-02-15 16:30:09 +0900

    [jpa2/API 개발 고급 - 지연 로딩과 조회 성능 최적화] 간단한 주문 조회 V1 엔티티를 직접 노출

---
+++ src/main/java/com/study/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,26 @@
+package com.study.jpashop.api;
+
+import com.study.jpashop.domain.Order;
+import com.study.jpashop.domain.OrderSearch;
+import com.study.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // LAZY 강제 초기화
+            order.getDelivery().getAddress(); // LAZY 강제 초기화
+        }
+        return all;
+    }
+}
