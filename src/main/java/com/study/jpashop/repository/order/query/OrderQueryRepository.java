715479af942c88f8ead7b0a605f15e8f45ad7a8a
bizplus033 <bizplus033@naver.com>
2024-02-16 13:08:14 +0900

    [jpa2/API 개발 고급 - 컬렉션 조회 최적화] 주문 조회 V5, 6

--- src/main/java/com/study/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/com/study/jpashop/repository/order/query/OrderQueryRepository.java
@@ -70,4 +70,16 @@ public class OrderQueryRepository {
                 .getResultList();
         return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new com.study.jpashop.repository.order" +
+                        ".query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
